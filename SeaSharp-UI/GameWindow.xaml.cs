acbb0ab6b1c020a9ea3c6b197e3d22fdd7aac778
Michael Navazhylau <mikipux7@gmail.com>
2021-07-22 12:17:40 -0500

    Creature assets and Window switching

---
+++ SeaSharp-UI/GameWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SeaSharp_UI
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        private string selectedCreature = null;
+
+        public GameWindow(string selectedCreature)
+        {
+            InitializeComponent();
+
+            this.selectedCreature = selectedCreature;
+            this.LogSelectedCreature();
+        }
+
+        void LogSelectedCreature()
+        {
+            System.Console.WriteLine("The selected creature is ");
+            System.Console.WriteLine(this.selectedCreature);
+        }
+
+    }
+}
--- SeaSharp-UI/MainWindow.xaml.cs
+++ SeaSharp-UI/MainWindow.xaml.cs
@@ -24,5 +24,52 @@ namespace SeaSharp_UI
         {
             InitializeComponent();
         }
+
+        private void SwitchToGameWindow(string creature)
+        {
+            Window gameWindow = new GameWindow(creature);
+
+            Window currentWindow = App.Current.MainWindow;
+            var oldLeftX = currentWindow.Left;
+            var oldTopY = currentWindow.Top;
+
+            gameWindow.WindowStartupLocation = WindowStartupLocation.Manual;
+
+            gameWindow.Left = oldLeftX;
+            gameWindow.Top = oldTopY;
+
+            App.Current.MainWindow = gameWindow;
+
+            this.Close();
+            gameWindow.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Control control = sender as Control;
+
+            Console.WriteLine($"Button Clicked: {control.Name}");
+
+            string creature = null;
+
+            switch (control.Name)
+            {
+                case "CrabBtn":
+                    creature = "crab";
+                    break;
+                case "DolphinBtn":
+                    creature = "dolphin";
+                    break;
+                case "SeahorseBtn":
+                    creature = "seahorse";
+                    break;
+                default:
+                    creature = "crab";
+                    break;
+            }
+
+            SwitchToGameWindow(creature);
+        }
+
     }
 }
